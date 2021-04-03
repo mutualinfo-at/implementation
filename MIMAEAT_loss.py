5132f80ad931205246813e14990871ad3a33d117
mutualinfo-at <plutarchp@yahoo.com>
2021-04-03 05:10:26 -0500

    first commit

---
+++ MIMAEAT_loss.py
@@ -0,0 +1,127 @@
+'''
+we used the computation of the Mutual information code fom the paper bellow 
+@inproceedings{ji2019invariant,
+    title={Invariant information clustering for unsupervised image classification and segmentation},
+    author={Ji, Xu and Henriques, Jo{\~a}o F and Vedaldi, Andrea},
+    booktitle={Proceedings of the IEEE International Conference on Computer Vision},
+    pages={9865--9874},
+    year={2019}
+}
+
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import sys
+
+def MI(x_out, x_tf_out, lamb=1.0, EPS=sys.float_info.epsilon):
+  # has had softmax applied
+  _, k = x_out.size()
+  p_i_j = compute_joint(x_out, x_tf_out)
+  assert (p_i_j.size() == (k, k))
+
+  p_i = p_i_j.sum(dim=1).view(k, 1).expand(k, k).clone()
+  p_j = p_i_j.sum(dim=0).view(1, k).expand(k, k).clone()  # but should be same, symmetric
+
+  # avoid NaN losses. Effect will get cancelled out by p_i_j tiny anyway
+  p_i_j[(p_i_j < EPS).data] = EPS
+  p_j[(p_j < EPS).data] = EPS
+  p_i[(p_i < EPS).data] = EPS
+
+  loss = - p_i_j * (torch.log(p_i_j) \
+                    - lamb * torch.log(p_j) \
+                    - lamb * torch.log(p_i))
+
+  loss = loss.sum()
+
+  loss_no_lamb = - p_i_j * (torch.log(p_i_j) \
+                            - torch.log(p_j) \
+                            - torch.log(p_i))
+
+  loss_no_lamb = loss_no_lamb.sum()
+
+  return loss #, loss_no_lamb
+
+
+def compute_joint(x_out, x_tf_out):
+  # produces variable that requires grad (since args require grad)
+
+  bn, k = x_out.size()
+  assert (x_tf_out.size(0) == bn and x_tf_out.size(1) == k)
+
+  p_i_j = x_out.unsqueeze(2) * x_tf_out.unsqueeze(1)  # bn, k, k
+  p_i_j = p_i_j.sum(dim=0)  # k, k
+  p_i_j = (p_i_j + p_i_j.t()) / 2.  # symmetrise
+  p_i_j = p_i_j / p_i_j.sum()  # normalise
+
+  return p_i_j
+
+
+
+
+def squared_l2_norm(x):
+    flattened = x.view(x.unsqueeze(0).shape[0], -1)
+    return (flattened ** 2).sum(1)
+
+
+def l2_norm(x):
+    return squared_l2_norm(x).sqrt()
+
+
+
+
+def MIAT3_loss(model,
+              x_natural,
+              y,
+              optimizer,
+              step_size=0.007,
+              epsilon=0.031,
+              perturb_steps=10,
+              beta= 0.4,
+              distance='l_inf'):
+    kl = nn.KLDivLoss(reduction='none')
+    l2 = nn.L1Loss()
+    model.eval()
+    batch_size = len(x_natural)
+    # generate adversarial example
+    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
+    if distance == 'l_inf':
+        for _ in range(perturb_steps):
+            x_adv.requires_grad_()
+            with torch.enable_grad():
+                loss_ce = F.cross_entropy(model(x_adv), y)
+            grad = torch.autograd.grad(loss_ce, [x_adv])[0]
+            x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
+            x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
+            x_adv = torch.clamp(x_adv, 0.0, 1.0)
+    else:
+        x_adv = torch.clamp(x_adv, 0.0, 1.0)
+    model.train()
+
+    x_adv = Variable(torch.clamp(x_adv, 0.0, 1.0), requires_grad=False)
+    # zero gradient
+    optimizer.zero_grad()
+
+    logits = model(x_natural)
+
+    logits_adv = model(x_adv)
+
+    adv_probs = F.softmax(logits_adv, dim=1)
+
+    tmp1 = torch.argsort(adv_probs, dim=1)[:, -2:]
+
+    new_y = torch.where(tmp1[:, -1] == y, tmp1[:, -2], tmp1[:, -1])
+
+    loss_adv = F.cross_entropy(logits_adv, y) + F.nll_loss(torch.log(1.0001 - adv_probs + 1e-12), new_y)
+
+    nat_probs = F.softmax(logits, dim=1)
+    
+    true_probs = torch.gather(nat_probs, 1, (y.unsqueeze(1)).long()).squeeze()
+
+    loss_robust = MI(torch.log(adv_probs+1e-12),nat_probs)
+    loss = loss_adv + beta*loss_robust + l2(logits_adv,logits)
+ 
+    
+    return loss
